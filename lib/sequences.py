ad96eb4f58b0f2aeebcead2e0a872c21ecf9dc73
rm0087 <michetti.ray@gmail.com>
2024-07-17 11:34:22 -0400

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
+# my_list = [1,2,3,4]
+# my_list.append({"num1":"1", "num2":"2"})
+# print(my_list[4]["num2"])
+
+# month_tuple = (
+#     'January',
+#     'February',
+#     'March',
+#     'April',
+#     'May',
+#     'June',
+#     'July',
+#     'August',
+#     'September',
+#     'October'
+#     'November'
+#     'December,'
+# )
+
+# print(f"The current month is: {month_tuple[7]}")
+
+# even_numbers_up_to_100 = range(0, 101, 2)
+# print(f"The number is: {even_numbers_up_to_100}")
+
+# s = [4, 6, 3, 9, 3, 5, 1, 2]
+# print(s.count(3))
+
 def print_fibonacci(length):
+    fibonacci = [0,1,1,2,3,5,8,13,21,34]
+    print(fibonacci[0:length])
     pass
+
+print_fibonacci(10)
